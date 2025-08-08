32487c255049994e7bffe1228a86d9232fff8daf
Jean Augusto <jean88asl@gmail.com>
2025-08-08 16:01:27 -0300

    remove cartshop

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,23 +1,21 @@
 import { globalStyles } from "../styles/global";
-import { Container, Header } from '../styles/pages/app'
+import { Container, Header } from "../styles/pages/app";
 import type { AppProps } from "next/app";
-import { FiShoppingCart } from "react-icons/fi"
+import { FiShoppingCart } from "react-icons/fi";
 // melhor local para ficar o globalStyles é aqui
-globalStyles()
-import logoImg from "../assets/logo.svg"
+globalStyles();
+import logoImg from "../assets/logo.svg";
 
 import Image from "next/image";
 
 import { CartProvider } from "use-shopping-cart";
-import { ShoppingModal } from "./hooks/shopping-cart"
+// import { ShoppingModal } from "./hooks/shopping-cart";
 
 export default function App({ Component, pageProps }: AppProps) {
+  const stripeKey = process.env.STRIPE_PUBLIC_KEY;
 
-  const stripeKey = process.env.STRIPE_PUBLIC_KEY
-
-  const successUrl = `${process.env.NEXT_URL}/success?session_id={CHECKOUT_SESSION_ID}`
-  const cancelUrl = `${process.env.NEXT_URL}`
-
+  const successUrl = `${process.env.NEXT_URL}/success?session_id={CHECKOUT_SESSION_ID}`;
+  const cancelUrl = `${process.env.NEXT_URL}`;
 
   return (
     // https://codingwithlucy.hashnode.dev/build-a-shopping-cart-in-nextjs-with-use-shopping-cart-and-stripe
@@ -35,7 +33,7 @@ export default function App({ Component, pageProps }: AppProps) {
         <Header>
           <Image src={logoImg} alt="" />
 
-          <ShoppingModal />
+          {/* <ShoppingModal /> */}
         </Header>
 
         <Component {...pageProps} />
